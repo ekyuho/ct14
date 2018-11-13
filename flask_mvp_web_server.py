9d3d24763b77264384ce917fe52de27459d30777
Kyuho Kim <ekyuho@gmail.com>
2018-11-14 02:27:30 +0900

    Update flask_mvp_web_server.py

--- flask_mvp_web_server.py
+++ flask_mvp_web_server.py
@@ -6,23 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open("log.txt", "a") as f:
-        dstr = datetime.datetime.now()
-        f.write("%s, " % (dstr))
-        str = ""
-        for p in request.args:
-            str += "[%s:%s],  " % (p, request.args[p])
-            f.write(str)
-        f.write("\n");
-        if str == "": str = "no parameters"
-        print('/', str, flush=True)
-    return 'I got: ' + str
+    return 'name=%s'%(request.args.get('name'))
 
 @app.route('/cakes')
 def cakes():
     print("/cakes", flush=True)
     return 'Yummy cakes!'
 
-print("Waiting at port 5000")
+print("Waiting at port 5000", flush=True)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
